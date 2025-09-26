1bcba8cea3b55b90b89a666fc638a6bc9af8c64e
ChengZhenLee <zhensstories@gmail.com>
2025-09-26 14:58:18 +0200

    Merge branch 'main' of https://github.com/ChengZhenLee/Trader

---
+++ client/src/components/signals/AllRequiredParamsBlock-TABLET-7EALSR14.jsx
@@ -0,0 +1,147 @@
+import { useEffect } from "react";
+import { SignalParametersConfig } from "../../configs/SignalSettingsConfig";
+import { useSelectedSignals } from "../../hooks/useSelectedSignals";
+import { useConfigContext } from "../../hooks/useConfigContext";
+
+export function AllRequiredParamsBlock() {
+    const { selectedSignals, _ } = useSelectedSignals();
+    const { config, setConfig } = useConfigContext();
+
+    const optimize = config.trader_settings.optimize || false;
+
+    useEffect(() => {
+        const inactiveParamsKey = optimize ? "all_signal_params" : "all_signal_optimize_params";
+
+        setConfig((prevConfig) => {
+            const signals = prevConfig.signals || {};
+            const inactiveParams = signals[inactiveParamsKey] || {};
+            if (Object.keys(inactiveParams).length === 0) {
+                return (prevConfig);
+            }
+            
+            return ({
+                ...prevConfig,
+                "signals": {
+                    ...signals,
+                    [inactiveParamsKey]: {}
+                }
+            });
+        });
+    }, [optimize, setConfig]);
+
+    return (
+        <div>
+            {selectedSignals.map((signal) => {
+                const paramsInfo = SignalParametersConfig.find(
+                    (elem) => elem.signalName === signal.name
+                );
+
+                if (!paramsInfo) return;
+<<<<<<< HEAD
+                
+                return (
+                    <div key={signal.name}>
+                        <RequiredParams paramsInfo={paramsInfo} />
+=======
+
+                return (
+                    <div key={signal.name}>
+                        {optimize ? 
+                        /*implement this component*/
+                        <RequiredOptimizeParams paramsInfo={paramsInfo} /> : 
+                        <RequiredParams paramsInfo={paramsInfo}/>}
+>>>>>>> 172ce3459787bc2a0315c6c19babf40df681d809
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
+function RequiredParams({ paramsInfo }) {
+<<<<<<< HEAD
+=======
+    const { _, setConfig } = useConfigContext();
+
+    // Set the default configs
+    useEffect(() => {
+        const paramsObject = paramsInfo.params.reduce((acc, param) => {
+            acc[param.name] = param.defaultValue;
+            return acc;
+        }, {});
+
+        setConfig((prevConfig) => {
+            const signals = prevConfig.signals || {};
+            const allSignalParameters = signals.all_signal_parameters || {};
+
+            return ({
+                ...prevConfig,
+                "signals": {
+                    ...signals,
+                    all_signal_parameters: {
+                        ...allSignalParameters,
+                        [paramsInfo.signalName]: paramsObject
+                    }
+                }
+            });
+        })
+    }, []);
+
+
+    // Update the value of a specific param of a signal
+    function updateConfigParameters(signalName, paramName, value) {
+        setConfig((prevConfig) => {
+            const signals = prevConfig.signals || {};
+            const allSignalParameters = signals.all_signal_parameters || {};
+            const signalParams = allSignalParameters[signalName] || {};
+
+            return ({
+                ...prevConfig,
+                "signals": {
+                    ...signals,
+                    all_signal_parameters: {
+                        ...allSignalParameters,
+                        [signalName]: {
+                            ...signalParams,
+                            [paramName]: value
+                        }
+                    }
+                }
+            });
+        });
+    }
+
+>>>>>>> 172ce3459787bc2a0315c6c19babf40df681d809
+    return (
+        <>
+            <div>Parameters for {paramsInfo.signalName}</div>
+            {paramsInfo.params.map((param) => {
+                return (
+                    <div key={param.name}>
+                        <div key={param.name}>
+                            {param.label}
+                        </div>
+                        <input
+                            name={param.name}
+                            type="number"
+                            step={param.type === "float" ? 0.01 : 1}
+                            defaultValue={param.defaultValue}
+<<<<<<< HEAD
+                            onChange={() => {
+                                
+=======
+                            onChange={(event) => {
+                                updateConfigParameters(
+                                    paramsInfo.signalName,
+                                    param.name,
+                                    event.target.value
+                                )
+>>>>>>> 172ce3459787bc2a0315c6c19babf40df681d809
+                            }}
+                        />
+                    </div>
+                );
+            })}
+        </>
+    );
+}
